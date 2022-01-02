d6ab09361d607ac92824bacf592970d45b76bfb6
Juan David Merchan Torres <juan1014166@gmail.com>
2022-01-01 22:35:46 -0500

    build(root): Change deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,11 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:NikolaM-Dev/counter-app-tsx.git main:gh-pages
 
 cd -
